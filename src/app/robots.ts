6006b5632d090c534bff36a5d62d46e3959672d7
johnerik510 <johnerik510@gmail.com>
2026-04-05 20:52:16 +0200

    SEO: GPTBot-block i robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,7 +2,12 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/" },
+    rules: [
+      { userAgent: "*", allow: "/" },
+      { userAgent: "GPTBot", disallow: "/" },
+      { userAgent: "ChatGPT-User", disallow: "/" },
+      { userAgent: "CCBot", disallow: "/" },
+    ],
     sitemap: "https://www.testahemma.se/sitemap.xml",
   };
 }
